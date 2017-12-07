8699a9b7b051c28407d6808e299386514954e727
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-12-06 21:15:06 -0800

    Update Legion build to explicitly depend on Python.

--- recipes/legion/build.sh
+++ recipes/legion/build.sh
@@ -1,10 +1,11 @@
-PYTHON_BIN="$(which python)"
-PYTHON_VER=$("$PYTHON_BIN" -c 'import sys; print("%s.%s" % (sys.version_info.major, sys.version_info.minor))')
-PYTHON_INC="$(dirname "$(dirname $PYTHON_BIN)")"/include
-PYTHON_LIB="$(dirname "$(dirname $PYTHON_BIN)")"/lib/libpython"$PYTHON_VER"m.so
-
 mkdir build
 cd build
-cmake -DBUILD_SHARED_LIBS=ON -DLegion_BUILD_BINDINGS=ON -DLegion_USE_Python=ON -DPYTHON_EXECUTABLE="$PYTHON_BIN" -DPYTHON_LIBRARY="$PYTHON_LIB" -DPYTHON_INCLUDE_DIR="$PYTHON_INC" -DPYTHONLIBS_VERSION_STRING="$PYTHON_VER" -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
+cmake \
+  -DBUILD_SHARED_LIBS=ON \
+  -DLegion_BUILD_BINDINGS=ON \
+  -DLegion_USE_Python=ON \
+  -DPYTHON_EXECUTABLE="$PYTHON" \
+  -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+  ..
 make -j12
 make install
