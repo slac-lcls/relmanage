655c5a49070959776acd1ed1595842c6ca0be91f
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-12-08 13:04:22 -0800

    Fix typo in Legion build.

--- recipes/legion/build.sh
+++ recipes/legion/build.sh
@@ -7,7 +7,7 @@ cmake \
   -DLegion_USE_Python=ON \
   -DPYTHON_EXECUTABLE="$PYTHON" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-  -DCMAKE_LIBRARY_OUTPUT_DIRECTORY="$PREFIX/lib" \
+  -DCMAKE_INSTALL_LIBDIR="$PREFIX/lib" \
   ..
 make -j12
 make install
