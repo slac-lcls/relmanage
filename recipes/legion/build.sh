4efd148f4dcb32f38eda4034987dd1a2b3e8bda4
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-12-08 11:48:25 -0800

    Build Legion with TLS support.

--- recipes/legion/build.sh
+++ recipes/legion/build.sh
@@ -3,6 +3,7 @@ cd build
 cmake \
   -DBUILD_SHARED_LIBS=ON \
   -DLegion_BUILD_BINDINGS=ON \
+  -DLegion_ENABLE_TLS=ON \
   -DLegion_USE_Python=ON \
   -DPYTHON_EXECUTABLE="$PYTHON" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
