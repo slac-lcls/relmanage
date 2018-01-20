eca6d5e2f9f59b55ef4033d6224d9803974c23e0
psrel <psrel@psbuild-rhel6.slac.stanford.edu>
2018-01-19 18:26:27 -0800

    add libfabric recipe since RHEL6 version is too old, and add it to default conda environment creation

---
+++ recipes/libfabric/build.sh
@@ -0,0 +1,15 @@
+# cpo tried this officially recommended autogen approach, but configure
+# complained on RHEL6, so try getting the .tar.gz file directly instead
+
+#./autogen.sh
+
+# configure.ac:297: error: possibly undefined macro: AC_CONFIG_FILES
+#      If this token and others are legitimate, please use m4_pattern_allow.
+#      See the Autoconf documentation.
+#configure:15917: error: possibly undefined macro: m4_ifnblank
+
+# feels like maybe the above needs a new version of some autoconf tools?
+
+./configure --enable-verbs --prefix=$PREFIX
+make -j2
+make install
