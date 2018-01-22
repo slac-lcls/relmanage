edad22c3376ebd19e019aee0cb9ca99f606bdb58
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2018-01-22 13:17:41 -0800

    keep a separate script for setting up the dedicated rhel7 environment

---
+++ bin/psconda7.sh
@@ -0,0 +1,7 @@
+lcls2conda="/reg/g/psdm/sw/conda2/inst"
+# the official way to pick up the modern compilers, but starts a shell :-(
+#scl enable devtoolset-7 "bash --rcfile ~psrel/.bashrc_lcls2"
+export PATH=$lcls2conda/bin:/opt/rh/devtoolset-7/root/usr/bin:$PATH
+unset LD_LIBRARY_PATH
+unset PYTHONPATH
+source activate ps-0.0.4
