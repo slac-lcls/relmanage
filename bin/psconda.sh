26263f7fab281ee9d7040cb61e9b5b4be61dd042
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2018-01-22 15:20:16 -0800

    fix up compiler path so it works on both rhel 6/7

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,7 +1,8 @@
 lcls2conda="/reg/g/psdm/sw/conda2/inst"
 # the official way to pick up the modern compilers, but starts a shell :-(
 #scl enable devtoolset-7 "bash --rcfile ~psrel/.bashrc_lcls2"
-export PATH=$lcls2conda/bin:/opt/rh/devtoolset-6/root/usr/bin:$PATH
+rhel_version=$(less /etc/redhat-release | grep -o -E '[0-9]+' | head -1)
+export PATH=$lcls2conda/bin:/opt/rh/devtoolset-$rhel_version/root/usr/bin:$PATH
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
 source activate ps-0.0.5
