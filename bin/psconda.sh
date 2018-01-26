44f4d9ed0a4b50620369b03b12cde2a649b0ef74
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2018-01-26 13:10:13 -0800

    make ps-0.0.6 the default

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -5,4 +5,4 @@ rhel_version=$(less /etc/redhat-release | grep -o -E '[0-9]+' | head -1)
 export PATH=$lcls2conda/bin:/opt/rh/devtoolset-$rhel_version/root/usr/bin:$PATH
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
-source activate ps-0.0.5
+source activate ps-0.0.6
