0ffcc98696492581504051291651912fbd70b421
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2018-02-28 18:58:35 -0800

    New way to activate environments, and add new packages

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,8 +1,8 @@
-lcls2conda="/reg/g/psdm/sw/conda2/inst"
 # the official way to pick up the modern compilers, but starts a shell :-(
 #scl enable devtoolset-7 "bash --rcfile ~psrel/.bashrc_lcls2"
 rhel_version=$(less /etc/redhat-release | grep -o -E '[0-9]+' | head -1)
-export PATH=$lcls2conda/bin:/opt/rh/devtoolset-$rhel_version/root/usr/bin:$PATH
+export PATH=/opt/rh/devtoolset-$rhel_version/root/usr/bin:$PATH
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
-source activate ps-0.0.6
+source /reg/g/psdm/sw/conda2/inst/etc/profile.d/conda.sh
+conda activate ps-0.0.6
