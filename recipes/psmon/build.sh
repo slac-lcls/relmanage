0163c8fc70f4219dc8865aebd03242edf37a7e47
psrel <psrel@psbuild-rhel6.slac.stanford.edu>
2018-04-02 14:58:37 -0700

    workaround for install_requires in setup.py making conda unhappy

--- recipes/psmon/build.sh
+++ recipes/psmon/build.sh
@@ -1,1 +1,4 @@
-python setup.py install
+# flags are a workaround for conda being unhappy with the
+# install_requires field in setup.py.  this tells setup.py
+# to ignore the install_requires.
+python setup.py install --single-version-externally-managed --record=/tmp/record.txt
