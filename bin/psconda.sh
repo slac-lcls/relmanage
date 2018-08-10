892b484cac03d3322dd1f4831745252fe11c5ec1
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2018-08-10 12:19:58 -0700

    add some new packages. pick up modern compilers without explicitly setting PATH in psconda.sh

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,7 +1,5 @@
-# the official way to pick up the modern compilers, but starts a shell :-(
-#scl enable devtoolset-7 "bash --rcfile ~psrel/.bashrc_lcls2"
 rhel_version=$(less /etc/redhat-release | grep -o -E '[0-9]+' | head -1)
-export PATH=/opt/rh/devtoolset-$rhel_version/root/usr/bin:$PATH
+source scl_source enable devtoolset-$rhel_version
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
 source /reg/g/psdm/sw/conda2/inst/etc/profile.d/conda.sh
