308d4e9558a0109c65b1d741e0349868e1308f85
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2018-08-10 12:33:02 -0700

    redirect devtoolset errors to /dev/null

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,5 +1,6 @@
 rhel_version=$(less /etc/redhat-release | grep -o -E '[0-9]+' | head -1)
-source scl_source enable devtoolset-$rhel_version
+# redirect errors to /dev/null to avoid errors on nodes without devtoolset
+source scl_source enable devtoolset-$rhel_version >& /dev/null
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
 source /reg/g/psdm/sw/conda2/inst/etc/profile.d/conda.sh
