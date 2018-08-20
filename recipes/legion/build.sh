0a70763eded0ba8d6d4a007875ccad1115cc025f
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-08-20 15:50:45 -0700

    Update Legion installation to include GASNet.

--- recipes/legion/build.sh
+++ recipes/legion/build.sh
@@ -1,3 +1,8 @@
+git clone https://github.com/StanfordLegion/gasnet.git
+pushd gasnet
+make CONDUIT=mpi
+popd
+
 mkdir build
 cd build
 cmake \
@@ -6,6 +11,8 @@ cmake \
   -DLegion_ENABLE_TLS=ON \
   -DLegion_USE_Python=ON \
   -DPYTHON_EXECUTABLE="$PYTHON" \
+  -DLegion_USE_GASNet=ON \
+  -DGASNet_ROOT_DIR="$PWD/../gasnet/release" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCMAKE_INSTALL_LIBDIR="$PREFIX/lib" \
   ..
