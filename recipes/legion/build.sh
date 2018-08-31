2531cd828a9099e1c28cb52a813937e87fe40610
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-08-31 15:14:20 -0700

    Make Legion not depend on MPI.

--- recipes/legion/build.sh
+++ recipes/legion/build.sh
@@ -1,19 +1,19 @@
-git clone https://github.com/StanfordLegion/gasnet.git
-pushd gasnet
-make CONDUIT=mpi
-popd
+# git clone https://github.com/StanfordLegion/gasnet.git
+# pushd gasnet
+# make CONDUIT=mpi
+# popd
 
 mkdir build
 cd build
+#   -DLegion_USE_GASNet=ON \
+#   -DGASNet_ROOT_DIR="$PWD/../gasnet/release" \
+#   -DGASNet_CONDUIT=mpi \
 cmake \
   -DBUILD_SHARED_LIBS=ON \
   -DLegion_BUILD_BINDINGS=ON \
   -DLegion_ENABLE_TLS=ON \
   -DLegion_USE_Python=ON \
   -DPYTHON_EXECUTABLE="$PYTHON" \
-  -DLegion_USE_GASNet=ON \
-  -DGASNet_ROOT_DIR="$PWD/../gasnet/release" \
-  -DGASNet_CONDUIT=mpi \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCMAKE_INSTALL_LIBDIR="$PREFIX/lib" \
   ..
