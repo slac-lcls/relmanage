7ca4dd28a84923f2461858f539989cb5527a5bc0
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-09-04 15:24:30 -0700

    Add CentOS build.

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 for PYVER in 2.7 3.6; do
-    tag=slaclcls/travis:ubuntu16.04-py$PYVER
-
     if [[ $PYVER == 2.7 ]]; then
         env_file=env_create_py2.yaml
     elif [[ $PYVER == 3.* ]]; then
         env_file=env_create.yaml
     fi
 
+    tag=slaclcls/travis:ubuntu16.04-py$PYVER
     docker build --build-arg CONDA_ENV_FILE=$env_file -t $tag -f docker/Dockerfile.ubuntu . && docker push $tag
+
+    tag=slaclcls/travis:centos6-py$PYVER
+    docker build --build-arg CONDA_ENV_FILE=$env_file -t $tag -f docker/Dockerfile.centos . && docker push $tag
 done
