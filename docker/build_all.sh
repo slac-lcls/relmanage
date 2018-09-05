9b8eee3e96320a4e373cb2f42807faea5064b7ab
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-09-05 10:07:28 -0700

    Refactor script to build images.

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-for PYVER in 2.7 3.6; do
-    if [[ $PYVER == 2.7 ]]; then
+function build_image {
+    os_name=$1
+    os_version=$2
+    py_version=$3
+
+    if [[ $py_version == 2.7 ]]; then
         env_file=env_create_py2.yaml
-    elif [[ $PYVER == 3.* ]]; then
+    elif [[ $py_version == 3.* ]]; then
         env_file=env_create.yaml
     fi
 
-    tag=slaclcls/travis:ubuntu16.04-py$PYVER
+    tag=slaclcls/travis:$os_name$os_version-py$py_version
     docker build --build-arg CONDA_ENV_FILE=$env_file -t $tag -f docker/Dockerfile.ubuntu . && docker push $tag
 
-    tag=slaclcls/travis:centos6-py$PYVER
-    docker build --build-arg CONDA_ENV_FILE=$env_file -t $tag -f docker/Dockerfile.centos . && docker push $tag
+}
+
+for py_version in 2.7 3.6; do
+    build_image ubuntu 16.04 $py_version
+    build_image centos 6 $py_version
 done
