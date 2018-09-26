92bc05391d6382aa4921b4ace3e2c24a41d9a8dc
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-09-26 13:52:38 -0700

    Make it possible to build CUDA and GASNet with Legion.

--- recipes/legion/build.sh
+++ recipes/legion/build.sh
@@ -1,13 +1,25 @@
-# git clone https://github.com/StanfordLegion/gasnet.git
-# pushd gasnet
-# make CONDUIT=mpi
-# popd
+CMAKE_FLAGS=()
+if [[ $USE_CUDA -eq 1 ]]; then
+    CMAKE_FLAG+=(
+	-DLegion_USE_CUDA=ON
+    )
+fi
+
+if [[ $USE_GASNET -eq 1 ]]; then
+    git clone https://github.com/StanfordLegion/gasnet.git
+    pushd gasnet
+    make CONDUIT=mpi
+    popd
+
+    CMAKE_FLAGS+=(
+	-DLegion_USE_GASNet=ON
+	-DGASNet_ROOT_DIR="$PWD/../gasnet/release"
+	-DGASNet_CONDUIT=mpi
+    )
+fi
 
 mkdir build
 cd build
-#   -DLegion_USE_GASNet=ON \
-#   -DGASNet_ROOT_DIR="$PWD/../gasnet/release" \
-#   -DGASNet_CONDUIT=mpi \
 cmake \
   -DBUILD_SHARED_LIBS=ON \
   -DLegion_BUILD_BINDINGS=ON \
@@ -16,6 +28,7 @@ cmake \
   -DPYTHON_EXECUTABLE="$PYTHON" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCMAKE_INSTALL_LIBDIR="$PREFIX/lib" \
+  "${CMAKE_FLAGS[@]}" \
   ..
 make -j12
 make install
