b587959d1a6a1f130eca47e9bd4da1aadacd3c2d
Clemens Weninger <weninc@slac.stanford.edu>
2018-10-25 14:49:41 -0700

    update epics-base recipe

--- recipes/epics-base/build.sh
+++ recipes/epics-base/build.sh
@@ -1,6 +1,19 @@
 EPICS_BASE=$PREFIX/epics
-make -j 8 INSTALL_LOCATION=$EPICS_BASE
+
 EPICS_HOST_ARCH=$(startup/EpicsHostArch)
+export EPICS_HOST_ARCH
+
+GNU_DIR=$(dirname $(dirname $(which x86_64-conda_cos6-linux-gnu-gcc)))
+if [ "$GNU_DIR" != "/usr" ]; then
+    echo "GNU_DIR="$GNU_DIR >> configure/CONFIG_COMMON
+    echo "CMPLR_PREFIX=x86_64-conda_cos6-linux-gnu-" >> configure/CONFIG_COMMON
+    sed -i  "s|USR_CXXFLAGS =|USR_CXXFLAGS = -std=c++11|" configure/CONFIG_COMMON
+    # sed -i  "s|USR_INCLUDES =|USR_INCLUDES = -I$CONDA_PREFIX/include|" configure/CONFIG_COMMON
+    # sed -i  "s|USR_LDFLAGS =|USR_LDFLAGS = -L$CONDA_PREFIX/lib -Wl,-rpath,$CONDA_PREFIX/lib|" configure/CONFIG_COMMON
+fi
+
+make -j $CPU_COUNT INSTALL_LOCATION=$EPICS_BASE
+
 mkdir -p $PREFIX/etc/conda/activate.d
 mkdir -p $PREFIX/etc/conda/deactivate.d
 ACTIVATE=$PREFIX/etc/conda/activate.d/epics_base.sh
