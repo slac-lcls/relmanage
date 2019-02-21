a5b38d37039431491acbd3bdb4b7415bd36fb632
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2019-02-21 15:29:19 -0800

    go back to separate yaml files to create conda py2/py3 envs

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -6,7 +6,11 @@ function build_image {
     py_version=$3
 
     env_file=temp.yaml
-    sed s/PYTHONVER/$py_version/ env_create.yaml > $env_file
+    if [ $py_version == 2.7 ]; then
+        sed s/PYTHONVER/$py_version/ env_create_py2.yaml > $env_file
+    else
+        sed s/PYTHONVER/$py_version/ env_create.yaml > $env_file
+    fi
 
     tag=slaclcls/travis:$os_name$os_version-py$py_version
     docker build --build-arg CONDA_ENV_FILE=$env_file -t $tag -f docker/Dockerfile.$os_name . && docker push $tag
