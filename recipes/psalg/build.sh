5bc885ace66e995414fc38bcf401ebb0ede0838c
Daniel Damiani <ddamiani@slac.stanford.edu>
2019-06-25 11:09:42 -0700

    add missing requirements for psana and build psalg python

--- recipes/psalg/build.sh
+++ recipes/psalg/build.sh
@@ -4,3 +4,5 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release ..
 make -j $CPU_COUNT
 make install
+cd ..
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
