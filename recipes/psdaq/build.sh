b854a0d908e353e0999f98dd0f4c8929cbb70013
Clemens Weninger <weninc@slac.stanford.edu>
2019-08-08 19:37:05 -0700

    update xtcdata and psdaq recipes

--- recipes/psdaq/build.sh
+++ recipes/psdaq/build.sh
@@ -1,7 +1,7 @@
 cd psdaq
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$PREFIX ..
 make -j $CPU_COUNT
 make install
 cd ..
