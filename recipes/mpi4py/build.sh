ff2838adc588423ce974a7b80bbe701a6dfc1afd
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2020-02-05 10:45:19 -0800

    update mpi4py recipe and start a rough script for creating releases

--- recipes/mpi4py/build.sh
+++ recipes/mpi4py/build.sh
@@ -8,7 +8,16 @@ mkdir -p $PREFIX/etc/conda/deactivate.d
 
 ACTIVATE=$PREFIX/etc/conda/activate.d/openmpi.sh
 
-echo 'rhel_version=$(less /etc/redhat-release | grep -o -E '[0-9]+' | head -1)' >> $ACTIVATE
-echo 'export PATH=/reg/common/package/openmpi/4.0.0-rhel$rhel_version/bin:$PATH' >> $ACTIVATE
-echo 'export LD_LIBRARY_PATH=/reg/common/package/openmpi/4.0.0-rhel$rhel_version/lib' >> $ACTIVATE
+# cpo - I believe we kept openmpi out of conda so we could support
+# multiple OS-dependent versions at runtime like this.
 
+# it's important that mpi4py only go into a local conda channel
+# (not the cloud) since it is facility-specific
+
+#echo 'rhel_version=$(less /etc/redhat-release | grep -o -E '[0-9]+' | head -1)' >> $ACTIVATE
+#echo 'export PATH=/reg/common/package/openmpi/4.0.0-rhel$rhel_version/bin:$PATH' >> $ACTIVATE
+#echo 'export LD_LIBRARY_PATH=/reg/common/package/openmpi/4.0.0-rhel$rhel_version/lib' >> $ACTIVATE
+
+# version built on rhel7 that with LSF and verbs options
+echo 'export LD_LIBRARY_PATH=/reg/common/package/openmpi/openmpi-4.0.2-verbs-lsf/install/lib' >> $ACTIVATE
+echo 'export PATH=/reg/common/package/openmpi/openmpi-4.0.2-verbs-lsf/install/bin:$PATH' >> $ACTIVATE
