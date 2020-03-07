bfac291d79f040a3401b3e89a8c6b8f701286ba3
monarin <monarin@gmail.com>
2020-03-06 18:38:33 -0800

    add -t option for packages

--- bin/make_release.sh
+++ bin/make_release.sh
@@ -3,19 +3,40 @@
 
 set -e
 
+helps="Usage: cmd [-h] [-t pkg,pkg] version" 
+
 if [ $# -eq 0 ]; then
-    echo "Usage: ./make_release.sh version"
-    echo "To auto build and upload lcls2 packages and create new conda environment with the specified version number."
+    echo $helps
     exit 0
 fi 
 
-pkgs=("xtcdata" "psalg" "psana" "psdaq" "ami")
+target="xtcdata,psalg,psana,psdaq,ami" # default packages
+
+while getopts "ht:" opt; do
+    case ${opt} in
+        h ) echo $helps
+            exit 0
+            ;;
+        t ) 
+            target=$OPTARG
+            ;;
+        \? ) echo $helps
+            exit 0
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+version=${1}
+
+if [ -z "$version" ]; then echo $helps; exit; fi
+
+IFS=',' read -ra pkgs <<< "$target"
 pyver="3.7"
 reldir="/reg/g/psdm/sw/conda2/manage/recipes/"
 channel="lcls-ii"
-version=${1}
 
-echo "Build lcls2 packages and upload to $channel"
+echo "Build $target and upload to $channel for release: $version"
 
 for pkg in "${pkgs[@]}"
 do
