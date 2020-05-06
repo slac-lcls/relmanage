90c7b2aac819b68a110bd2ff92c79656e2f56850
Valerio Mariani <valmar@slac.stanford.edu>
2020-05-06 09:01:29 -0700

    Updated libfabric recipe to 1.10.0

--- recipes/libfabric/build.sh
+++ recipes/libfabric/build.sh
@@ -12,6 +12,7 @@
 
 cp ib_user_verbs.h include/rdma/
 #./configure --enable-sockets --enable-verbs --enable-usnic --disable-static --enable-psm --enable-psm2 --prefix=${PREFIX}
+./autogen.sh
 ./configure --enable-verbs --prefix=${PREFIX}
 ${BUILD_PREFIX}/bin/make -j ${CPU_COUNT}
 ${BUILD_PREFIX}/bin/make install
