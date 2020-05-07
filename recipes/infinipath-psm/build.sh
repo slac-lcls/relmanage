67de24a34f715b5e18d4e46671dbb55ca48182f9
Valerio Mariani <valmar@slac.stanford.edu>
2020-05-07 11:14:38 -0700

    Removed unneeded dependencies

--- recipes/infinipath-psm/build.sh
+++
@@ -1,5 +0,0 @@
-${BUILD_PREFIX}/bin/make PSM_USE_SYS_UUID=1 -j ${CPU_COUNT}
-DESTDIR=${PREFIX} libdir=/lib ${BUILD_PREFIX}/bin/make install
-mv ${PREFIX}/usr/include ${PREFIX}
-rmdir ${PREFIX}/usr
-
--- recipes/libpsm2/build.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p include/rdma/hfi
-cp hfi1_user.h include/rdma/hfi
-export CFLAGS="-fno-strict-aliasing -Wno-int-in-bool-context -Wno-format-truncation -Wno-sizeof-array-argument"
-${BUILD_PREFIX}/bin/make CCARCH=gcc -j ${CPU_COUNT}
-DESTDIR=${PREFIX} LIBDIR=/lib ${BUILD_PREFIX}/bin/make install
