136505d4936e19f63135809f7d74cf86e453686c
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2020-05-13 08:41:25 -0700

    move to conda compilers

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,20 +1,8 @@
-rhel_version=$(less /etc/redhat-release | grep -o -E '[0-9]+' | head -1)
-
-# redirect errors to /dev/null to avoid errors on nodes without devtoolset
-# this created build errors in jenkins for not-understood reasons
-#source scl_source enable devtoolset-$rhel_version >& /dev/null
-
-export PATH=/opt/rh/devtoolset-$rhel_version/root/usr/bin:$PATH
-
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
 source /reg/g/psdm/sw/conda2/inst/etc/profile.d/conda.sh
-
-# user can pick up python2 by adding "-py2" option to setup_env.sh
-conda activate ps-2.1.2$@
+conda activate ps-3.1.7
 
 if [ ! -d /usr/share/X11/xkb ]; then
     export QT_XKB_CONFIG_ROOT=${CONDA_PREFIX}/lib
 fi
-
-export MANPATH=$CONDA_PREFIX/share/man${MANPATH:+:${MANPATH}}
