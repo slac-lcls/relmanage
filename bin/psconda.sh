da93c7f2886794965cab610ad5eb2007b495b0f0
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2020-05-19 18:39:54 -0700

    pin rogue, switch default version in psconda.sh, add git

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,7 +1,7 @@
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
 source /reg/g/psdm/sw/conda2/inst/etc/profile.d/conda.sh
-conda activate ps-3.1.7
+conda activate ps-3.1.10
 
 if [ ! -d /usr/share/X11/xkb ]; then
     export QT_XKB_CONFIG_ROOT=${CONDA_PREFIX}/lib
