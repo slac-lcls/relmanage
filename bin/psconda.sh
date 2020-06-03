6d8a0fb47637e997064e91de4104131873437a7f
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2020-06-03 15:06:12 -0700

    put back in manpath in psconda.sh

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,8 +1,11 @@
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
 source /reg/g/psdm/sw/conda2/inst/etc/profile.d/conda.sh
-conda activate ps-3.1.10
+conda activate ps-3.1.11
 
 if [ ! -d /usr/share/X11/xkb ]; then
     export QT_XKB_CONFIG_ROOT=${CONDA_PREFIX}/lib
 fi
+
+# needed by Ric to get correct libfabric man pages
+export MANPATH=$CONDA_PREFIX/share/man${MANPATH:+:${MANPATH}}
