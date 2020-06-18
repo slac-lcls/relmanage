5d4aa1d07bb212c77799334b42d0956c9d5bb947
Daniel Damiani <dsdamiani@gmail.com>
2020-06-18 15:09:49 -0700

    update to amityping 1.1.3

--- recipes/amityping/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# checkout the tag matching the package version
-git checkout "$PKG_VERSION"
-# call setup.py
-$PYTHON setup.py install  --single-version-externally-managed --record=record.txt
