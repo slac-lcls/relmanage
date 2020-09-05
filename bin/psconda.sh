4459812f8ac9de82bf8fdacf3b5afa9fe1e21495
psrel <psrel@psbuild-rhel7-01.slac.stanford.edu>
2020-09-04 18:43:37 -0700

    add new pyfftw dep for ami, fix lcls2-timetool recipe

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,11 +1,8 @@
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
 source /reg/g/psdm/sw/conda2/inst/etc/profile.d/conda.sh
-conda activate ps-3.1.16
+conda activate ps-3.0.14
 
 if [ ! -d /usr/share/X11/xkb ]; then
     export QT_XKB_CONFIG_ROOT=${CONDA_PREFIX}/lib
 fi
-
-# needed by Ric to get correct libfabric man pages
-export MANPATH=$CONDA_PREFIX/share/man${MANPATH:+:${MANPATH}}
