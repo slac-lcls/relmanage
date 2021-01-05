58070e2efcd505817c65a59ef8aa59ae7bb46f94
Valerio Mariani <valmar@slac.stanford.edu>
2021-01-05 15:56:55 -0800

    update activation script to 4.0.9

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -4,7 +4,7 @@ source /cds/sw/ds/ana/conda2/inst/etc/profile.d/conda.sh
 # kludge: somehow conda activate is executing rogue stuff before
 # LD_LIBRARY_PATH is set to pick up the ugly location of epics
 export LD_LIBRARY_PATH=/cds/sw/ds/ana/conda2/inst/envs/ps-4.0.4/epics/lib/linux-x86_64:/cds/sw/ds/ana/conda2/inst/envs/ps-4.0.4/pcas/lib/linux-x86_64
-conda activate ps-4.0.8
+conda activate ps-4.0.9
 
 if [ ! -d /usr/share/X11/xkb ]; then
     export QT_XKB_CONFIG_ROOT=${CONDA_PREFIX}/lib
