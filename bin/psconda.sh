0ff9ea377c853da6ba25274636895eb0c71a60c4
Valerio Mariani <valmar@slac.stanford.edu>
2021-03-24 16:49:31 -0700

    OpenBLAS thread limitation and recipe updates

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -6,6 +6,9 @@ source /cds/sw/ds/ana/conda2/inst/etc/profile.d/conda.sh
 export LD_LIBRARY_PATH=/cds/sw/ds/ana/conda2/inst/envs/ps-4.0.4/epics/lib/linux-x86_64:/cds/sw/ds/ana/conda2/inst/envs/ps-4.0.4/pcas/lib/linux-x86_64
 conda activate ps-4.0.9
 
+# cpo: seems that in more recent versions blas is creating many threads
+export OPENBLAS_NUM_THREADS=1
+
 if [ ! -d /usr/share/X11/xkb ]; then
     export QT_XKB_CONFIG_ROOT=${CONDA_PREFIX}/lib
 fi
