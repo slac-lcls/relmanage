b21a4c8a01ab0dcac0171dae667fbee74e0d57b9
Valerio Mariani <valmar@slac.stanford.edu>
2021-12-16 16:16:12 -0800

    Moved to version 4.4.4

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -4,7 +4,7 @@ source /cds/sw/ds/ana/conda2/inst/etc/profile.d/conda.sh
 # kludge: somehow conda activate is executing rogue stuff before
 # LD_LIBRARY_PATH is set to pick up the ugly location of epics
 #export LD_LIBRARY_PATH=/cds/sw/ds/ana/conda2/inst/envs/ps-4.4.1/epics/lib/linux-x86_64:/cds/sw/ds/ana/conda2/inst/envs/ps-4.4.1/pcas/lib/linux-x86_64
-conda activate ps-4.4.3
+conda activate ps-4.4.4
 
 # cpo: seems that in more recent versions blas is creating many threads
 export OPENBLAS_NUM_THREADS=1
