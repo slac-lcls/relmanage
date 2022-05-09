b0fd599e7286ef255e05e51117986538793f5017
Valerio Mariani <valmar@slac.stanford.edu>
2022-05-09 14:56:30 -0700

    support for mamba solver in psconda.sh

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,6 +1,22 @@
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
-source /cds/sw/ds/ana/conda2/inst/etc/profile.d/conda.sh
+
+v2=0
+for arg in "$@"
+do
+  if [ "$arg" == "-v2" ]
+  then
+    v2=1
+  fi
+done
+if [ $v2 -eq 1 ]
+then
+   source /cds/sw/ds/ana/conda2-v2/inst/etc/profile.d/conda.sh
+   export CONDA_ENVS_DIRS=/cds/sw/ds/ana/conda2/inst/envs/ 
+else
+   source /cds/sw/ds/ana/conda2/inst/etc/profile.d/conda.sh
+fi
+
 # kludge: somehow conda activate is executing rogue stuff before
 # LD_LIBRARY_PATH is set to pick up the ugly location of epics
 #export LD_LIBRARY_PATH=/cds/sw/ds/ana/conda2/inst/envs/ps-4.4.1/epics/lib/linux-x86_64:/cds/sw/ds/ana/conda2/inst/envs/ps-4.4.1/pcas/lib/linux-x86_64
